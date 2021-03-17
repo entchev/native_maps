da57f0981900cee5d9735d86941edf053dd1e814
entchev <micishi@gmail.com>
2021-03-17 15:01:17 +0000

    first commit

---
+++ screens/PlaceDetailScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+const PlacesDetailScreen = (props) => {
+  return (
+    <View>
+      <Text>Places Detail Screen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
+
+export default PlacesDetailScreen
