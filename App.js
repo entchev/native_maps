b8925037b32489406cd1ee022e69a6e5fd8dd69c
entchev <micishi@gmail.com>
2021-03-19 21:52:07 +0000

    reset

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
-import React from 'react';
-
-import PlacesNavigator from './navigation/PlacesNavigator';
+import React from 'react'
+import { View } from 'react-native'
 
 export default function App() {
-  return <PlacesNavigator />;
+  return <View></View>
 }
